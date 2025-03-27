a5abce62e79e0f44d2815c215afbdef9694e54e7
Domingo Ramírez Morales <128168900+Rzmsdo@users.noreply.github.com>
2025-03-27 13:36:56 +0000

    p

---
+++ src/README.md
@@ -0,0 +1,8 @@
+# prueba_tecnica_midudev
+
+#Proyecto de aprendizaje 
+
+Siguiendo video de Midudev en youtube realice a la par esta prueba tecnica.
+
+
+
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import * as ReactDom from "react-dom/client"
+import "./index.css";
+
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<h1>Hola Domi</h1>);
+
+
+
